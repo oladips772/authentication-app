738fb0c117126a730d041c93712542313c82cc3f
oladips <oladips200@gmail.com>
2022-08-04 21:06:18 +0000

    adding contents to homepage

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,24 +1,101 @@
 /** @format */
-import React from "react";
-import phoneImage from "../images/crypto-phone.png";
+import { useEffect, useState } from "react";
+import userPng from "../images/user.png";
+import moneyPng from "../images/money.png";
+import userPng from "../images/user.png";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import "animate.css/animate.min.css";
+import axios from "axios";
+import currencyFormatter from "currency-formatter";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 function Main() {
+  const [coins, setCoins] = useState([]);
+  const URL =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=ngn&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+  const getCoins = async () => {
+    const response = await axios.get(URL);
+    console.log(response.data);
+    setCoins(response.data);
+  };
+
+  useEffect(() => {
+    // getCoins();
+  }, []);
+
   return (
     <main className="mt-[80px]">
-      <div className="flex items-center">
-        <h2 className="text-[60px] font-[500]">
-          Jump start your <br /> crypto career by investing with us.
+      <div className="flex flex-col items-center">
+        <h2 className="text-[66px] font-[600] leading-[80px] m-4 text-center">
+          Jump start your crypto career by investing with us.
         </h2>
-        <img src={phoneImage} alt="" className="h-[480px] w-[380px]" />
+        <div className="my-6">
+          <button className="text-lg font-[600] h-[60px] w-[200px] border-2 border-blue-600 text-black">
+            Sign in{" "}
+          </button>
+          <button className="text-lg font-[600] h-[60px] w-[200px] bg-blue-700 text-white">
+            {" "}
+            Get started
+          </button>
+        </div>
       </div>
-      <div>
-        <h1>The world fastest crypto investment app</h1>
-        {/* <AnimationOnScroll animateIn="animate__bounceIn"> */}
-        {/* </AnimationOnScroll> */}
+      {/* COINS DIV */}
+      <div className="bg-white border border-slate-300 mb-10 mt-4">
+        <div className="flex justify-between items-center p-4 border-b border-slate-300">
+          <h4 className="mx-2 font-[500] text-lg text-gray-600">Name</h4>
+          <h4 className="mx-2 font-[500] text-lg text-gray-600">Price</h4>
+          <h4 className="mx-2 font-[500] text-lg text-gray-600">Change</h4>
+        </div>
+        {coins?.slice(0, 10).map((coin) => (
+          <div className="flex justify-between items-center border-b border-slate-200 p-4 mx-4">
+            <div className="flex items-center space-x-4 ">
+              <img src={coin.image} alt="" className="h-[35px] w-[35px]" />
+              <h4 className="font-[500] text-[20px]">{coin.name}</h4>
+              <h4 className="text-gray-500 font-[600]">
+                {coin.symbol.toUpperCase()}
+              </h4>
+            </div>
+            <div className="flex">
+              <h5 className="text-lg font-[600]">
+                {currencyFormatter.format(coin.current_price, { code: "NGN" })}
+              </h5>
+            </div>
+            <h5>{coin.ath_change_percentage}</h5>
+          </div>
+        ))}
       </div>
-      {/*  */}
+      {/* world div */}
+      <div className="my-8">
+        <h1 className="text-[60px] text-center font-[800] leading-[75px]">
+          The World's Fastest Growing Crypto Investment Platform{" "}
+        </h1>
+        <div className="space-y-2 mt-4">
+          <div className="flex items-center space-x-4 justify-center">
+            <CheckCircleIcon className="text-blue-800" />
+            <h4 className="text-2xl font-[500]">
+              Join 50m+ users buying and selling 250+ cryptocurrencies at true
+              cost
+            </h4>
+          </div>
+          <div className="flex items-center space-x-4 justify-center">
+            <CheckCircleIcon className="text-blue-800" />
+            <h4 className="text-2xl font-[500]">
+              Join 50m+ users buying and selling 250+ cryptocurrencies at true
+              cost
+            </h4>
+          </div>{" "}
+          <div className="flex items-center space-x-4 justify-center">
+            <CheckCircleIcon className="text-blue-800" />
+            <h4 className="text-2xl font-[500]">
+              Join 50m+ users buying and selling 250+ cryptocurrencies at true
+              cost
+            </h4>
+          </div>
+        </div>
+      </div>
+      {/*  get started div*/}
+      <div></div>
     </main>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,11 +13,11 @@ function Navbar() {
   };
 
   return (
-    <nav 
+    <nav
       className={`${
         !showNav
-          ? "bg-white h-[60px] w-full shadow-md flex items-center justify-between md:justify-around lg:justify-around fixed top-0 z-10"
-          : "flex-col items-center space-y-2 bg-white h-[100%] shadow-md fixed top-0 z-10 w-[100%]"
+          ? "bg-white h-[60px] w-full shadow-sm border-b border-slate-200 flex items-center justify-between md:justify-around lg:justify-around fixed top-0 z-10"
+          : "flex-col items-center space-y-2 bg-white h-[100%] shadow-sm border-b border-slate-300  fixed top-0 z-10 w-[100%]"
       } `}
     >
       {/* logo and icon div */}
@@ -29,8 +29,8 @@ function Navbar() {
         }`}
       >
         {/* <img src={logo} className="h-40 w-40 resize-contain" alt="" /> */}
-        <h3 className="text-3xl text-blue-900 font-[500]">coinbox</h3>
-        <div className="flex md:hidden lg:hidden mr-4" >
+        <h3 className="text-[26px] text-blue-900 font-[500]">coinbox</h3>
+        <div className="flex md:hidden lg:hidden mr-4">
           {showNav ? (
             <CloseIcon
               className="cursor-pointer"
@@ -46,8 +46,8 @@ function Navbar() {
       </div>
       {/* links div */}
       <motion.div
-      animate={showNav ? "open" : "closed"}
-      variants={variants}
+        animate={showNav ? "open" : "closed"}
+        variants={variants}
         className={`${
           !showNav
             ? "items-center space-x-4 hidden md:flex lg:flex"
@@ -58,8 +58,8 @@ function Navbar() {
           to="/"
           className={` ${
             !showNav
-              ? "font-[600] mx-4 hover:text-blue-600"
-              : "border-b border-slate-200 font-[600] mx-4 hover:text-blue-600 w-[96%] pb-1"
+              ? "font-[500] mx-1 text-[15px] hover:text-blue-600"
+              : "border-b border-slate-200 font-[500] mx-4 hover:text-blue-600 w-[96%] pb-1"
           }`}
         >
           About
@@ -68,8 +68,8 @@ function Navbar() {
           to="/"
           className={` ${
             !showNav
-              ? "font-[600] mx-4 hover:text-blue-600"
-              : "border-b border-slate-200 font-[600] mx-4 hover:text-blue-600 w-[96%] pb-1"
+              ? "font-[500] mx-4 text-[15px] hover:text-blue-600"
+              : "border-b border-slate-200 font-[500] mx-4 hover:text-blue-600 w-[96%] pb-1"
           }`}
         >
           Plans
@@ -78,8 +78,8 @@ function Navbar() {
           to="/"
           className={` ${
             !showNav
-              ? "font-[600] mx-4 hover:text-blue-600"
-              : "border-b border-slate-200 font-[600] mx-4 hover:text-blue-600 w-[96%] pb-1"
+              ? "font-[500] mx-4 text-[15px] hover:text-blue-600"
+              : "border-b border-slate-200 font-[500] mx-4 hover:text-blue-600 w-[96%] pb-1"
           }`}
         >
           Blogs
@@ -88,8 +88,8 @@ function Navbar() {
           to="/"
           className={` ${
             !showNav
-              ? "font-[600] mx-4 hover:text-blue-600"
-              : "border-b border-slate-200 font-[600] mx-4 hover:text-blue-600 w-[96%] pb-1"
+              ? "font-[500] mx-4 text-[15px] hover:text-blue-600"
+              : "border-b border-slate-200 font-[500] mx-4 hover:text-blue-600 w-[96%] pb-1"
           }`}
         >
           Contact
@@ -97,8 +97,8 @@ function Navbar() {
       </motion.div>
       {/* actions */}
       <motion.div
-      animate={showNav ? "open" : "closed"}
-      variants={variants}
+        animate={showNav ? "open" : "closed"}
+        variants={variants}
         className={`${
           !showNav
             ? "items-center space-x-4 hidden md:flex lg:flex"
@@ -109,8 +109,8 @@ function Navbar() {
           to="/"
           className={` ${
             !showNav
-              ? "font-[600] mx-4"
-              : "font-[600] mx-4 p-[8px] border-2 border-blue-600 rounded-[4px] text-center max-w-[290px] w-full"
+              ? "font-[500] mx-4 text-[15px]"
+              : "font-[500] mx-4 p-[8px] border-2 border-blue-600 rounded-[4px] text-center max-w-[290px] w-full"
           }  `}
         >
           Sign in
@@ -119,7 +119,7 @@ function Navbar() {
           to="/"
           className={`${
             !showNav
-              ? "font-[600] bg-blue-700 text-white p-[8px] rounded-[4px]"
+              ? "font-[500] bg-blue-700 text-white h-[36px] p-[2px] text-[15px] w-[98px] flex justify-center items-center rounded-[4px]"
               : "font-[500] bg-blue-700 text-white p-[8px] rounded-[4px] text-center max-w-[290px] w-full"
           }`}
         >

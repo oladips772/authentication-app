a15f93a6d7d802a79f8c143a9ea0fdc1815f3d1e
oladips <oladips200@gmail.com>
2022-08-08 20:59:40 +0000

    gettimg blog details

--- frontend/src/components/DetailLoader.js
+++ frontend/src/components/DetailLoader.js
@@ -1,17 +1,17 @@
 /** @format */
 import React from "react";
 
-function BlogLoader() {
+function DetailLoader() {
   return (
     <div>
       <div className="mt-[60px]">
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 max-w-[1070px] mx-auto mb-8">
+        <div className=" max-w-[1070px] mx-auto mb-8">
           <div className="hover:shadow-md border-md mx-2 h-full animate-pulse">
-            <div className="h-[230px] bg-slate-200 p-2"></div>
-            <h1 className="text-[25px] mb-2 font-[700] p-2 h-4 w-full mt-6 bg-slate-200 "></h1>
+            <div className="h-[400px] bg-slate-200 p-2"></div>
+            <h1 className="text-[25px] mb-2 font-[700] p-2 h-8 w-full mt-6 bg-slate-200 "></h1>
             <div className="flex items-center text-gray-600 font-[600] mt-2">
-              <p className="h-4 bg-slate-200 w-[200px]"> </p>{" "}
-              <span className="ml-8 h-4 w-[120px] bg-slate-200 "></span>
+              <p className="h-8 bg-slate-200 w-[200px]"> </p>{" "}
+              <span className="ml-8 h-8 w-[120px] bg-slate-200 "></span>
             </div>
           </div>
         </div>
@@ -20,4 +20,4 @@ function BlogLoader() {
   );
 }
 
-export default BlogLoader;
+export default DetailLoader;
--- frontend/src/pages/BlogDetail.js
+++ frontend/src/pages/BlogDetail.js
@@ -1,12 +1,31 @@
 /** @format */
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 function BlogDetail() {
   let params = useParams();
-    const blogId = params.id;
-    
+  const blogId = params.id;
+  const [blog, setBlog] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getBlog = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(`/api/news/${blogId}`);
+      console.log(data);
+      setBlog(data);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBlog();
+  }, []);
+
   return <div>BlogDetail</div>;
 }
 
